8da3f8d674697e39c22444f353df9230c324d102
AndersonBatalha <andersonpbatalha@gmail.com>
2019-02-06 19:42:43 -0200

    Criado um componente 'card' do Bootstrap para exibir as vagas disponíveis

--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,17 @@
     style="margin-top: 70px;"
   >
     <!-- seu código aqui -->
-    <div
-      v-for="(job, i) in jobs"
-      :key="i"
-    >
-      {{ job }}
+    <div v-for="(job, i) in jobs" :key="i" id='vagas'>
+      
+      <div class='card'>
+        <img :src='job.url_banner' class='card-img-top' alt='logo-empresa'/>
+        <div class='card-body'>
+          <h5 class='card-title'>{{ job.nome_empresa }} - {{ job.vaga }}</h5>
+          <a href='#' class='btn btn-lg btn-block'>CONHECER</a>
+        </div>
+      </div>
     </div>
+
   </div>
 </template>
 
